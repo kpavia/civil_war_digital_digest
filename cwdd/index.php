5417a4c12b5864071e52ab593ce1b1dfd62613f9
Kenneth Pavia <kennethpavia@gmail.com>
2016-05-03 19:38:20 -0600

    5/3/16 @ 7:38pm

--- cwdd/index.php
+++ cwdd/index.php
@@ -15,7 +15,13 @@
             </nav>
             <section id="main">
                 <p>This is the home page for the Civil War Digital Digest.</p>
-                <p>This is a test page.</p>
+                <p>Welcome to our site. It is currently under construction, but please come back often for updates.</p>
+                <p>In the meantime, please visit our YouTube page. There you can see amazing content bound to satisfy the young student of history to the seasoned educational professional.<br>
+                    While you're there, please subscribe, like us and share our videos.<br>
+                    </p>
+                    <p>Here is the introductory video to the Civil War Digital Digest. Enjoy!</p>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/XkTll5DyPbM" frameborder="0" allowfullscreen></iframe><br><br>
+                <a href="https://www.youtube.com/channel/UCR419HGD33ONzukp3c-WNPA/playlists">Click here to visit our playlist on YouTube</a>
             </section>
             <footer>
                 
