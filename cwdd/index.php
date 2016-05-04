28c9bcb97d4a4b8be4b0feff4dd881ee23e73f8a
Kenneth Pavia <kennethpavia@gmail.com>
2016-05-03 18:57:53 -0600

    5/3/16 @ 6:57pm

---
+++ cwdd/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en-us">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="author" content="Look Around You Ventures, LLC" />
+        <title>Civil War Digital Digest</title>
+    </head>
+    <body>
+        <div class="container">
+            <header>
+                
+            </header>
+            <nav>
+                
+            </nav>
+            <section id="main">
+                <p>This is the home page for the Civil War Digital Digest.</p>
+                <p>This is a test page.</p>
+            </section>
+            <footer>
+                
+            </footer>
+            
+        </div>
+    </body>
+</html>
