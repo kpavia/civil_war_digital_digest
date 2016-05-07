28a9302f031ba682f4be6c6ea529281d08d605a2
Kenneth Pavia <kennethpavia@gmail.com>
2016-05-07 14:36:52 -0600

    5/7/2016 @ 2:36pm

--- cwdd/index.php
+++ cwdd/index.php
@@ -4,27 +4,28 @@
         <meta charset="utf-8" />
         <meta name="author" content="Look Around You Ventures, LLC" />
         <title>Civil War Digital Digest</title>
+        <link type="text/css" rel="stylesheet" href="css/mainStylesheet.css" media="screen" >
     </head>
     <body>
         <div class="container">
             <header>
-                
+                <img src="images/cwddBanner.jpg" alt="cwddBanner" id="mainBanner" />
             </header>
             <nav>
                 
             </nav>
             <section id="main">
-                <p>This is the home page for the Civil War Digital Digest.</p>
-                <p>Welcome to our site. It is currently under construction, but please come back often for updates.</p>
-                <p>In the meantime, please visit our YouTube page. There you can see amazing content bound to satisfy the young student of history to the seasoned educational professional.<br>
-                    While you're there, please subscribe, like us and share our videos.<br>
-                    </p>
-                    <p>Here is the introductory video to the Civil War Digital Digest. Enjoy!</p>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/XkTll5DyPbM" frameborder="0" allowfullscreen></iframe><br><br>
-                <a href="https://www.youtube.com/channel/UCR419HGD33ONzukp3c-WNPA/playlists">Click here to visit our playlist on YouTube</a>
+                <p>Welcome to the web home of Civil War Digest Digest! We are in building now! As with any construction, there will be a period of mess, dust and spare parts.<br><br> 
+                   Please bear with us!<br><br>
+                   While we construct this site to enhance your historic experience on the web, please feel free to come visit us over at our YouTube channel. You can get there by clicking
+                   on the following link:<br><br>
+                   <a href="https://www.youtube.com/c/CivilWarDigitalDigest">Civil War Digital Digest YouTube Channel</a><br><br>
+                   Be sure to check back here regularly (and after about June 15) as we will be rolling out new ways to enjoy the history of the American Civil War.
+                </p>
+                 
             </section>
             <footer>
-                
+                <?php include $_SERVER['DOCUMENT_ROOT'].'/php_modules/footer/footer.php';?>
             </footer>
             
         </div>
