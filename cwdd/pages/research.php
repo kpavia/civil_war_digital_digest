6c0dea558542a61ef02f3ed54d27231a333b9791
Kenneth Pavia <kennethpavia@gmail.com>
2016-07-07 19:37:51 -0600

    7/7/16 @ 1937

---
+++ cwdd/pages/research.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="/css/second-bootstrap-stylesheet.css" >
+  <link href='https://fonts.googleapis.com/css?family=IM+Fell+DW+Pica:400,400italic' rel='stylesheet' type='text/css'>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <title>Civil War Digital Digest</title>
+  <script>
+    $(document).ready(function() {
+            $("#home").removeClass("active");
+            $("#episodes").removeClass("active");
+            $("#affiliations").removeClass("active");
+            $("#store").removeClass("active");
+            $("#support").removeClass("active");
+            $("#communicate").removeClass("active");
+            
+            $("#research").addClass("active");
+            
+    }); //end ready function
+  </script>
+</head>
+    <body>
+        <header>
+            <div class="container-fluid" >
+                <img src="/images/cwddBanner.jpg" class="img-responsive" id="banner-img"> 
+            </div>
+        </header>
+        <nav class="navbar navbar-inverse" >
+            <!-- this does the mobile device/smaller screen menu button -->
+            <div class="container-fluid">
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                </div>
+            </div>
+            
+            <div class="container-fluid" id="navbar-container">
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav">
+                        <li class="active" id="home"><a href="http://kspavia.com/bootstrap.php">Home</a></li>
+                        <li id="episodes"><a href="http://civilwardigitaldigest.com/pages/episodes.php">Episodes</a></li>
+                        <li id="research"><a href="http://civilwardigitaldigest.com/pages/research.php">Research</a></li>
+                        <li id="store"><a href="http://civilwardigitaldigest.com/pages/store.php">Store</a></li>
+                        <li id="support"><a href="http://civilwardigitaldigest.com/pages/support.php">Support</a></li>
+                        <li id="communicate"><a href="http://civilwardigitaldigest.com/pages/communicate.php">Communicate</a></li>
+                        <li id="affiliations"><a href="http://civilwardigitaldigest.com/pages/affiliations.php">Affiliations</a></li>
+                    </ul>
+                </div>
+            </div>
+            
+        </nav>
+        <div class="container-fluid text-center">
+            <div class="row content" id="content-ads-holding-div">
+                <aside class="col-sm-2 sidenav" id="left-side-aside">
+                        <p><a href="http://www.pastreflectionsreproductions.com/" media="screen, handheld" target="_blank"><img src="/images/past-reflections-ad.jpg" class="img-responsive"></a></p>
+                        <p><a href="https://www.etsy.com/shop/VictorianNeedle" media="screen, handheld" target="_blank"><img src="/images/vict-needle-ad.jpg" class="img-responsive"></a></p>         
+                </aside>
+                <section class="col-sm-8 text-left">
+                    <h1>Welcome to the Civil War Digital Digest</h1>
+                    <p>This is the research page. It is currently under construction.</p><br>
+                        <div id="intro-video-div">
+                            <iframe id="intro-video" src="https://www.youtube.com/embed/XkTll5DyPbM" frameborder="0" allowfullscreen></iframe>
+                        </div>
+                    <br><br>
+                    <p>Feel free to visit the episodes page of our site to view more content on the period of the American Civil War, and please follow the link to our YouTube channel where you
+                        can enjoy the Civil War Digital Digest's content on the period of the American Civil War and subscribe to the channel.
+                    </p>
+                    <br><br>
+                    <p><a href="https://www.youtube.com/channel/UCR419HGD33ONzukp3c-WNPA/videos" media="screen, handheld" target="_blank">Civil War Digital Digest YouTube Channel </a></p>
+                </section>
+                <aside class="col-sm-2 sidenav" id="right-side-aside">
+                    
+                        <p><a href="http://www.wwandcompany.com/" media="screen, handheld" target="_blank"><img src="/images/wambaugh-ad.jpg" class="img-responsive"></a>
+                    
+                    
+                        <p><a href="http://www.historicfortwaynecoalition.com/" media="screen, handheld" target="_blank"><img src="/images/hist-ft-wayne-co-ad.jpg" class="img-responsive"></a></p>
+                    
+                    
+                </aside>
+            </div>
+        </div>
+
+        <footer class="container-fluid text-center">
+            <p>&copy; 2016, Look Around You Ventures, LLC<br>
+                Site built by Crow Design Studios<br>
+                Last updated 7 May 2016
+            </p>
+        </footer>
+
+    </body>
+</html>
