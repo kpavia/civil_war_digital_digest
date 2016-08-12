ab20d5fc2cce78209ee6343d93b433ea8fefb044
Kenneth Pavia <kennethpavia@gmail.com>
2016-08-11 21:24:35 -0600

    8/11/2016 @ 2124

--- cwdd/future-home.php
+++ cwdd/future-home.php
@@ -4,17 +4,13 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="css/second-bootstrap-stylesheet.css" >
+  <link rel="stylesheet" type="text/css" href="css/side-by-side-homepage.css" >
   <link href='https://fonts.googleapis.com/css?family=IM+Fell+DW+Pica:400,400italic' rel='stylesheet' type='text/css'>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
   <title>Civil War Digital Digest</title>
   <script>
-      $(document).ready(function() {
-            
-            $("#home").addClass("active");
-            
-    }); //end ready function
+      
   </script>
 </head>
     <body>
@@ -23,80 +19,67 @@
                 <img src="images/cwddBanner.jpg" class="img-responsive" id="banner-img">
             </div>
         </header>
-        <nav class="navbar navbar-inverse" >
-            <!-- this does the mobile device/smaller screen menu button -->
-            <div class="container-fluid">
-                <div class="navbar-header">
-                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                  </button>
-                </div>
-            </div>
-            
-            <div class="container-fluid" id="navbar-container">
-                <div class="collapse navbar-collapse" id="myNavbar">
-                    <div id="nav-div-center">
-                        <ul class="nav navbar-nav" id="nav-ul">
-                            <li class="active" id="home"><a href="http://kspavia.com/bootstrap.php">Home</a></li>
-                            <li id="episodes"><a href="http://civilwardigitaldigest.com/pages/episodes.php">Episodes</a></li>
-                            <li id="research"><a href="http://civilwardigitaldigest.com/pages/research.php">Research</a></li>
-                            <li id="store"><a href="http://civilwardigitaldigest.com/pages/store.php">Store</a></li>
-                            <li id="support"><a href="http://civilwardigitaldigest.com/pages/support.php">Support</a></li>
-                            <li id="communicate"><a href="http://civilwardigitaldigest.com/pages/communicate.php">Communicate</a></li>
-                            <li id="links"><a href="http://civilwardigitaldigest.com/pages/affiliations.php">Links</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            
-        </nav>
+        
         <div class="container-fluid text-center">
             <div class="row content" id="content-ads-holding-div">
                 <aside class="col-sm-2 sidenav" id="left-side-aside">
-                       <p>
-                           <a href="https://www.etsy.com/shop/VictorianNeedle" media="screen, handheld" target="_blank">
-                               <img src="images/vict-needle-ad.jpg" class="img-responsive" id="vict-needle-ad">
-                           </a>
-                       </p>        
+                    <p>
+                        <a href="https://www.etsy.com/shop/VictorianNeedle" media="screen, handheld" target="_blank">
+                            <img src="/images/vict-needle-side-ad.jpg" class="img-responsive" id="vict-needle-ad">
+                        </a>
+                    </p>
                 </aside>
-                <section class="col-sm-8 text-left">
+                <section class="col-sm-8 text-left" id="main-content">
                     <h1>Welcome to the Civil War Digital Digest</h1>
-                    <p>Civil War Digital Digest is your first stop for high quality videos on American Civil War era history and living history topics.</p>
-                    <p>Avid living historians, relaxed armchair generals and teachers will find resources that both educate and entertain.</p>
-                        <div id="intro-video-div">
-                            <iframe id="intro-video" src="https://www.youtube.com/embed/XkTll5DyPbM" frameborder="0" allowfullscreen></iframe>
-                        </div>
+                    <p>Civil War Digital Digest is your first stop for high quality videos on American Civil War era history and living history topics.
+                        Avid living historians, relaxed armchair generals and teachers will find resources that both educate and entertain.
+                    </p>
+                    <br>
+                    <a href="http://civilwardigitaldigest.com/pages/episodes.php" media="screen, handheld">
+                        <img class="img-responsive menu-buttons" id="episodes-menu-button" src="/images/episodes-button.jpg" alt="episodes menu button">
+                    </a>
+                    <a href="http://civilwardigitaldigest.com/pages/research.php" media="screen, handheld">
+                        <img class="img-responsive menu-buttons" id="research-menu-button" src="/images/research-button.jpg" alt="research menu button" >
+                    </a>
+                    <br><br>
+                    <img class="img-responsive" id="jeremy-homepage-photo" src="/images/jeremy-front.jpeg" alt="Jeremy cooking on camera">
+                    <br>
+                    <a href="http://civilwardigitaldigest.com/pages/store.php" media="screen, handheld">
+                        <img class="img-responsive menu-buttons" id="store-menu-button" src="/images/store-button.jpg" alt="store episodes menu button">
+                    </a>
+                    <a href="http://civilwardigitaldigest.com/pages/support.php" media="screen, handheld">
+                        <img class="img-responsive menu-buttons" id="support-menu-button" src="/images/support-button.jpg" alt="support menu button">
+                    </a>
+                    <br><br>
+                    <img class="img-responsive" id="will-homepage-photo" src="/images/will-bottom.jpeg" alt="Will on a TV screen">
+                    <br>
+                    <a href="http://civilwardigitaldigest.com/pages/communicate.php" media="screen, handheld">
+                        <img class="img-responsive menu-buttons" id="communicate-menu-button" src="/images/communicate-button.jpg" alt="communicate menu button">
+                    </a>
+                    <a href="http://civilwardigitaldigest.com/pages/links.php" media="screen, handheld">
+                        <img class="img-responsive menu-buttons" id="links-menu-button" src="/images/links-button.jpg" alt="links menu button">
+                    </a>
+                    <div id="intro-video-div">
+                        <iframe id="intro-video" src="https://www.youtube.com/embed/XkTll5DyPbM" frameborder="0" allowfullscreen></iframe>
+                    </div>
                     <br><br>
                     
                     <a href="https://www.youtube.com/channel/UCR419HGD33ONzukp3c-WNPA/videos" media="screen, handheld" target="_blank"><img src="images/YouTube-logo-full_color.png" class="act                      img-responsive" id="youtube-logo" /></a>
+                    <br><br>
+                    
                 </section>
                 <aside class="col-sm-2 sidenav" id="right-side-aside">
-                    
-                        <p>
-                            <a href="http://www.wwandcompany.com/" media="screen, handheld" target="_blank">
-                                <img src="images/wambaugh-ad.jpg" class="img-responsive" id="wambaugh-ad-pic">
-                            </a>
-                        </p>
-                    
+                    <p>
+                        <a href="http://www.wwandcompany.com/" media="screen, handheld" target="_blank">
+                            <img src="/images/wambaugh-side-ad.jpg" class="img-responsive" id="wambaugh-ad-pic">
+                        </a>
+                    </p>
                 </aside>
             </div>
         </div>
 
         <footer class="container-fluid text-center">
             <?php include $_SERVER['DOCUMENT_ROOT'].'/php_modules/footer/footer.php'; ?>
-            <!-- Google Analytics Code -->
-            <script>
-                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-                ga('create', 'UA-81700293-1', 'auto');
-                ga('send', 'pageview');
-
-            </script>
         </footer>
 
     </body>
--- cwdd/grid-home.php
+++ cwdd/grid-home.php
@@ -25,59 +25,53 @@
                 <aside class="col-sm-2 sidenav" id="left-side-aside">
                     <p>
                         <a href="https://www.etsy.com/shop/VictorianNeedle" media="screen, handheld" target="_blank">
-                            <img src="/images/vict-needle-ad.jpg" class="img-responsive" id="vict-needle-ad">
+                            <img src="/images/vict-needle-side-ad.jpg" class="img-responsive" id="vict-needle-ad">
                         </a>
                     </p>
                 </aside>
                 <section class="col-sm-8 text-left">
                     <h1>Welcome to the Civil War Digital Digest</h1>
-                    <p>Civil War Digital Digest is your first stop for high quality videos on American Civil War era history and living history topics.</p>
-                    <p>Avid living historians, relaxed armchair generals and teachers will find resources that both educate and entertain.</p>
-                        <div id="intro-video-div">
-                            <iframe id="intro-video" src="https://www.youtube.com/embed/XkTll5DyPbM" frameborder="0" allowfullscreen></iframe>
-                        </div>
+                    <p>Civil War Digital Digest is your first stop for high quality videos on American Civil War era history and living history topics.
+                        Avid living historians, relaxed armchair generals and teachers will find resources that both educate and entertain.
+                    </p>
+                    <br>
+                    <a href="http://civilwardigitaldigest.com/pages/episodes.php" media="screen, handheld">
+                        <img class="img-responsive menu-buttons" id="episodes-menu-button" src="/images/episodes-button.jpg" alt="episodes menu button">
+                    </a>
+                    <a href="http://civilwardigitaldigest.com/pages/research.php" media="screen, handheld">
+                        <img class="img-responsive menu-buttons" id="research-menu-button" src="/images/research-button.jpg" alt="research menu button" >
+                    </a>
+                    <br>
+                    <img class="img-responsive" id="jeremy-homepage-photo" src="/images/jeremy-front.jpeg" alt="Jeremy cooking on camera">
+                    <br>
+                    <a href="http://civilwardigitaldigest.com/pages/store.php" media="screen, handheld">
+                        <img class="img-responsive menu-buttons" id="store-menu-button" src="/images/store-button.jpg" alt="store episodes menu button">
+                    </a>
+                    <a href="http://civilwardigitaldigest.com/pages/support.php" media="screen, handheld">
+                        <img class="img-responsive menu-buttons" id="support-menu-button" src="/images/support-button.jpg" alt="support menu button">
+                    </a>
+                    <br>
+                    <img class="img-responsive" id="will-homepage-photo" src="/images/will-bottom.jpeg" alt="Will on a TV screen">
+                    <br>
+                    <a href="http://civilwardigitaldigest.com/pages/communicate.php" media="screen, handheld">
+                        <img class="img-responsive menu-buttons" id="communicate-menu-button" src="/images/communicate-button.jpg" alt="communicate menu button">
+                    </a>
+                    <a href="http://civilwardigitaldigest.com/pages/links.php" media="screen, handheld">
+                        <img class="img-responsive menu-buttons" id="links-menu-button" src="/images/links-button.jpg" alt="links menu button">
+                    </a>
+                    <div id="intro-video-div">
+                        <iframe id="intro-video" src="https://www.youtube.com/embed/XkTll5DyPbM" frameborder="0" allowfullscreen></iframe>
+                    </div>
                     <br><br>
                     
                     <a href="https://www.youtube.com/channel/UCR419HGD33ONzukp3c-WNPA/videos" media="screen, handheld" target="_blank"><img src="images/YouTube-logo-full_color.png" class="act                      img-responsive" id="youtube-logo" /></a>
                     <br><br>
-                    <div class="menu container-fluid" >
-                        <ul id="grid-menu-ul">
-                            <li class="grid-menu-ul-li">
-                                <a href="http://civilwardigitaldigest.com/pages/episodes.php">
-                                    <img class="img-responsive button" id="grid-episodes" src="images/episodes-button.jpg" alt="episodes menu button">
-                                </a>
-                            </li>
-                            <li class="grid-menu-ul-li">
-                                <a href="http://civilwardigitaldigest.com/pages/research.php">
-                                    <img class="img-responsive button" id="grid-research" src="images/research-button.jpg" alt="research menu button">
-                                </a>
-                            </li>
-                            <li class="grid-menu-ul-li">
-                                <a href="http://civilwardigitaldigest.com/pages/store.php">
-                                    <img class="img-responsive button" id="grid-store" src="images/store-button.jpg" alt="store menu button" >
-                                </a>
-                            </li>
-                            <li class="grid-menu-ul-li">
-                                <a href="http://civilwardigitaldigest.com/pages/support.php">
-                                    <img class="img-responsive button" id="grid-support" src="images/support-button.jpg" alt="support menu button" >
-                                </a>
-                            </li>
-                            <li class="grid-menu-ul-li">
-                                <a href="http://civilwardigitaldigest.com/pages/communicate.php">
-                                    <img class="img-responsive button" id="grid-communicate" src="images/communicate-button.jpg" alt="communicate menu button" >
-                                </a>
-                            </li>
-                            <li class="grid-menu-ul-li">
-                                <a href="http://civilwardigitaldigest.com/pages/links.php">
-                                    <img class="img-responsive button" id="grid-links" src="images/links-button.jpg" alt="links menu button" >
-                                </a>
-                            </li>
-                    </div>
+                    
                 </section>
                 <aside class="col-sm-2 sidenav" id="right-side-aside">
                     <p>
                         <a href="http://www.wwandcompany.com/" media="screen, handheld" target="_blank">
-                            <img src="/images/wambaugh-ad.jpg" class="img-responsive" id="wambaugh-ad-pic">
+                            <img src="/images/wambaugh-side-ad.jpg" class="img-responsive" id="wambaugh-ad-pic">
                         </a>
                     </p>
                 </aside>
