4b4246bbf7f9d824b389a74b184692ca64f11d1d
Kenneth Pavia <kennethpavia@gmail.com>
2018-09-23 17:39:41 -0600

    patreon redirect page added

---
+++ cwdd/pages/patreon-intro.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"> 
+  <link rel="stylesheet" type="text/css" href="/css/patreon-intro.css" >
+  <link href='https://fonts.googleapis.com/css?family=IM+Fell+DW+Pica:400,400italic' rel='stylesheet' type='text/css'>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="/javascript/slug-line-popup.js"></script>
+  <title>Civil War Digital Digest</title>
+  <!-- Google Analytics Code -->
+  <script>
+                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+                ga('create', 'UA-81785309-2', 'auto');
+                ga('send', 'pageview');
+
+  </script>
+  <script>
+      $(document).ready(function() {
+            
+            $("#support").addClass("active");
+            
+    }); //end ready function
+  </script>
+</head>
+    <body>
+        <header>
+            <div class="container-fluid" >
+                <img src="/images/cwddBanner.jpg" class="img-responsive" id="banner-img">
+            </div>
+        </header>
+        <?php include $_SERVER['DOCUMENT_ROOT'].'/php_modules/nav/nav-bar.php'; ?>
+        <div class="container-fluid text-center">
+            <div class="row content" id="content-ads-holding-div">
+                <aside class="col-sm-2 sidenav" id="left-side-aside">
+                    
+                <!-- Responsive Adsense -->
+                    
+                    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                    
+                    <!-- CWDD responsive -->
+                    
+                        <ins class="adsbygoogle"
+                            style="display:block"
+                            data-ad-client="ca-pub-5786123947739131"
+                            data-ad-slot="6275650007"
+                            data-ad-format="auto"> 
+                        </ins>
+                    
+                    <script>
+                        (adsbygoogle = window.adsbygoogle || []).push({});
+                    </script>  
+                </aside>
+                <section class="col-sm-8 text-left">
+                    <div class="container-fluid episodes" id="main-content">
+                        <br>
+                        <img src="/images/hold-my-horse/eagle.jpg" class="img-responsive text-center"/>
+                        <br>
+                        <p>This video is available to our Patrons only.<br>
+                            As a group, they are called the CWDD Coffee Grinders and their support helps historic preservation as well as our efforts here.<br>
+                            We'd love to have you consider joining the CWDD Coffee Grinders!<br>
+                            You will get access to all premium videos, as well as other awards based on your level of support.<br>
+                            Click <a href="https://www.patreon.com/civilwardigitaldigest" target="_blank">here</a> to learn more!
+                        </p>
+                        
+                        
+                        
+                    
+                    </div>
+                </section>
+                <aside class="col-sm-2 sidenav" id="right-side-aside">
+                    
+                    <!-- Responsive Adsense -->
+                    
+                    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                    
+                    <!-- CWDD responsive -->
+                    
+                        <ins class="adsbygoogle"
+                            style="display:block"
+                            data-ad-client="ca-pub-5786123947739131"
+                            data-ad-slot="6275650007"
+                            data-ad-format="auto"> 
+                        </ins>
+                    
+                    <script>
+                        (adsbygoogle = window.adsbygoogle || []).push({});
+                    </script>   
+                        
+                </aside>
+            </div>
+        </div>
+        <footer class="container-fluid text-center">
+            <?php include $_SERVER['DOCUMENT_ROOT'].'/php_modules/footer/footer.php'; ?>
+        </footer>
+    </body>
+</html>
