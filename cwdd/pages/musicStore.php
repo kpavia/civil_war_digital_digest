91261754cdf2d30e5b5ca1d834c68406662f999d
Kenneth Pavia <kennethpavia@gmail.com>
2019-03-24 11:37:08 -0600

    music store

---
+++ cwdd/pages/musicStore.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"> 
+  <link rel="stylesheet" type="text/css" href="/css/music.css" >
+  <link href='https://fonts.googleapis.com/css?family=IM+Fell+DW+Pica:400,400italic' rel='stylesheet' type='text/css'>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="/javascript/slug-line-popup.js"></script>
+  <title>Civil War Digital Digest - Music Store</title>
+  <!-- Google Analytics Code -->
+  <script>
+               
+
+  </script>
+  <script>
+      $(document).ready(function() {
+            
+            $("#episodes-dropdown").addClass("active");
+            
+    }); //end ready function
+  </script>
+</head>
+    <body>
+        <header>
+            <div class="container-fluid" >
+                <img src="/images/cwddBanner.jpg" class="img-responsive" id="banner-img">
+            </div>
+        </header>
+        <?php include $_SERVER['DOCUMENT_ROOT'].'/php_modules/nav/nav-bar.php'; ?>
+        <div class="container-fluid text-center">
+            <div class="row content" id="content-ads-holding-div">
+                <aside class="col-sm-2 sidenav" id="left-side-aside">       
+                </aside>
+                <section class="col-sm-8 text-left">
+                    <div class="container-fluid episodes text-center">
+                        <h1 class="text-center">CWDD Music Store</h1>
+                        <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post" >
+                            <input type="hidden" name="cmd" value="_s-xclick">
+                            <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIG1QYJKoZIhvcNAQcEoIIGxjCCBsICAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYAzZ7uSg5lV/mRe6OcEJZwXYk/03BetTdDJMhRZPLcGWV9btg11Kxxfvcx+K9xOCSA46ogTeRMUrAb79sODrZaHe2N4uHMzkg9KfjrxBRk0hBkLGgc2HpYAtn3oZ8ELd7v/IBw2yo4cC3Hq88ieFkQ3jadKeF9/61uxgcyRXD/bgDELMAkGBSsOAwIaBQAwUwYJKoZIhvcNAQcBMBQGCCqGSIb3DQMHBAgwDnxTpBudE4AwmTdYTSOoP0qTw/nhVMuABMq7JPiDqDIbhpmEuO/9vy4hcBlXDXIOojG4H5aoYvhPoIIDhzCCA4MwggLsoAMCAQICAQAwDQYJKoZIhvcNAQEFBQAwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMB4XDTA0MDIxMzEwMTMxNVoXDTM1MDIxMzEwMTMxNVowgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBR07d/ETMS1ycjtkpkvjXZe9k+6CieLuLsPumsJ7QC1odNz3sJiCbs2wC0nLE0uLGaEtXynIgRqIddYCHx88pb5HTXv4SZeuv0Rqq4+axW9PLAAATU8w04qqjaSXgbGLP3NmohqM6bV9kZZwZLR/klDaQGo1u9uDb9lr4Yn+rBQIDAQABo4HuMIHrMB0GA1UdDgQWBBSWn3y7xm8XvVk/UtcKG+wQ1mSUazCBuwYDVR0jBIGzMIGwgBSWn3y7xm8XvVk/UtcKG+wQ1mSUa6GBlKSBkTCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb22CAQAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCBXzpWmoBa5e9fo6ujionW1hUhPkOBakTr3YCDjbYfvJEiv/2P+IobhOGJr85+XHhN0v4gUkEDI8r2/rNk1m0GA8HKddvTjyGw/XqXa+LSTlDYkqI8OwR8GEYj4efEtcRpRYBxV8KxAW93YDWzFGvruKnnLbDAF6VR5w/cCMn5hzGCAZowggGWAgEBMIGUMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTkwMzI0MTcxOTE4WjAjBgkqhkiG9w0BCQQxFgQU/kT/7l1N6dTTeXYdC8PsFfugK4AwDQYJKoZIhvcNAQEBBQAEgYC8Kiguqo3FfyJ8Mlo1wcHnjt6Dl4551kxsm9yO/0cujYDLL8brnTNIitK5iuxSujF5OOCG86Uz6nGIL+XNVvk/H1zinuTcwfVthZlwSFKhbLqojzg25dJ8ii1XPouDUlpZ5p/enrUI6ze4nIKM4cGbJhoB1YuX8dgHlD5bzuxqNw==-----END PKCS7-----">
+                            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_viewcart_LG.gif" border="0" name="submit" 
+                                   alt="PayPal - The safer, easier way to pay online!">
+                            <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                        </form>
+
+                        <table class="text-center">
+                            <tr>
+                                <th>Song</th>
+                                <th>Artist</th>
+                                <th>Price</th>
+                                <th>Purchase</th>
+                            </tr>
+                            <tr>
+                                <td><a style="font-size: 20px;" href="/audio/03 Friends and Lovers.wma">Friends & Lovers</a></td>
+                                <td>Incubus</td>
+                                <td>$0.01</td>
+                                <td>
+                                    <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+                                        <input type="hidden" name="cmd" value="_s-xclick">
+                                        <input type="hidden" name="hosted_button_id" value="BKLGCPE5U5K7E">
+                                        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" 
+                                            alt="PayPal - The safer, easier way to pay online!">
+                                        <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                                    </form>
+
+                                </td>
+                            </tr>
+                            
+                        </table>
+                        
+                        
+                        
+                        
+                        
+                    
+                    </div>
+                </section>
+                <aside class="col-sm-2 sidenav" id="right-side-aside">       
+                </aside>
+            </div>
+        </div>
+        <footer class="container-fluid text-center">
+            <?php include $_SERVER['DOCUMENT_ROOT'].'/php_modules/footer/footer.php'; ?>
+        </footer>
+    </body>
+</html>
