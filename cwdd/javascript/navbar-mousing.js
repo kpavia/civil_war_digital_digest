a908035fd157ddae1ffb6bfddefc00b14c23ac89
Kenneth Pavia <kennethpavia@gmail.com>
2019-09-20 18:00:10 -0600

    b4 sandbox built up to navbar

---
+++ cwdd/javascript/navbar-mousing.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+    $("#home-dropdown").mouseover(function(){
+        $("#home-dropdown").css("color", "white");
+    });
+    $("#home-dropdown").mouseout(function(){
+        $("#home-dropdown").css("color", "black");
+    });
+    
+    $("#episodes-dropdown").mouseover(function(){
+        $("#episodes-dropdown").css("color", "white");
+    });
+    $("#episodes-dropdown").mouseout(function(){
+        $("#episodes-dropdown").css("color", "black");
+    });
+    
+    $("#research-dropdown").mouseover(function(){
+        $("#research-dropdown").css("color", "white");
+    });
+    $("#research-dropdown").mouseout(function(){
+        $("#research-dropdown").css("color", "black");
+    });
+    
+    $("#store-dropdown").mouseover(function(){
+        $("#store-dropdown").css("color", "white");
+    });
+    $("#store-dropdown").mouseout(function(){
+        $("#store-dropdown").css("color", "black");
+    });
+    
+    $("#support-dropdown").mouseover(function(){
+        $("#support-dropdown").css("color", "white");
+    });
+    $("#support-dropdown").mouseout(function(){
+        $("#support-dropdown").css("color", "black");
+    });
+    
+    $("#communicate-dropdown").mouseover(function(){
+        $("#communicate-dropdown").css("color", "white");
+    });
+    $("#communicate-dropdown").mouseout(function(){
+        $("#communicate-dropdown").css("color", "black");
+    });
+    
+    $("#links-dropdown").mouseover(function(){
+        $("#links-dropdown").css("color", "white");
+    });
+    $("#links-dropdown").mouseout(function(){
+        $("#links-dropdown").css("color", "black");
+    });
+    
+});
+
---
+++ cwdd/php_modules/nav/b4-nav-bar.php
@@ -0,0 +1,44 @@
+<nav id="nav-bar" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 navbar navbar-expand-md">
+    <button class="navbar-toggler navbar-light" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a id="home-dropdown" href="http://civilwardigitaldigest.com/" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a id="episodes-dropdown" class="dropdown-toggle nav-link" data-toggle="dropdown" href="#">Episodes</a>
+                <ul class="dropdown-menu">
+                    <li><a href="http://civilwardigitaldigest.com/pages/episodes/volumeOne.php">Volume I (2015)</a></li>
+                    <li><a href="http://civilwardigitaldigest.com/pages/episodes/volumeTwo.php">Volume II (2016)</a></li>
+                    <li><a href="http://civilwardigitaldigest.com/pages/episodes/volumeThree.php">Volume III (2017)</a></li>
+                    <li><a href="http://civilwardigitaldigest.com/pages/episodes/volumeFour.php">Volume IV (2018)</a></li>
+                    <li><a href="http://civilwardigitaldigest.com/pages/episodes/v5-episodes.php">Volume V (2019)</a></li>
+                </ul>
+            </li>
+            <li class="nav-item dropdown">
+                <a id="research-dropdown" class="dropdown-toggle nav-link" data-toggle="dropdown" href="#" >Research</a>
+                <ul class="dropdown-menu">
+                    <li><a href="http://civilwardigitaldigest.com/pages/research/research-v1.php">Volume I (2015)</a></li>
+                    <li><a href="http://civilwardigitaldigest.com/pages/research/research-v2.php">Volume II (2016)</a></li>
+                    <li><a href="http://civilwardigitaldigest.com/pages/research/research-v3.php">Volume III (2017)</a></li>
+                    <li><a href="http://civilwardigitaldigest.com/pages/research/research-v4.php">Volume IV (2018)</a></li>
+                    <li><a href="http://civilwardigitaldigest.com/pages/research/research-v5.php">Volume V (2019)</a></li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a id="store-dropdown" href="http://civilwardigitaldigest.com/pages/store.php" target="_blank" class="nav-link">Store</a>
+            </li>
+            <li class="nav-item">
+                <a id="support-dropdown" href="http://civilwardigitaldigest.com/pages/support.php" class="nav-link">Support</a>
+            </li>
+            <li class="nav-item">
+                <a id="communicate-dropdown" href="http://civilwardigitaldigest.com/pages/communicate.php" class="nav-link">Communicate</a>
+            </li>
+            <li class="nav-item">
+                <a id="links-dropdown" href="http://civilwardigitaldigest.com/pages/links.php" class="nav-link">Links</a>
+            </li>
+        </ul>
+    </div>
+</nav>
